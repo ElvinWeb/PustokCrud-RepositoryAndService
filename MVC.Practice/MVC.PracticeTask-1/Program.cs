cc18a44792c14e0972b8583bd823f8a788470c56
llammann <bd7n2v9tr.@code.edu.az>
2023-12-07 17:21:12 +0400

    fixed some error

--- MVC.Practice/MVC.PracticeTask-1/Program.cs
+++ MVC.Practice/MVC.PracticeTask-1/Program.cs
@@ -57,12 +57,12 @@ app.MapRazorPages();
 
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
 );
 app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area=exists}/{controller=Home}/{action=Index}/{id?}"
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
 );
 
 app.Run();
